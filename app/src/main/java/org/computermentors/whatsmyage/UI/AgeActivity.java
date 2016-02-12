d7d763659fc9c17f93a51795ffe2d47a1a14c81b
Casoclash <casoclash@hotmail.com>
2016-02-11 19:26:46 -0500

    Revert "Created back button"
    This reverts commit fcdefd5b8666b1e5d74acf48ac4b7a9a0a3796a2.

--- app/src/main/java/org/computermentors/whatsmyage/UI/AgeActivity.java
+++ app/src/main/java/org/computermentors/whatsmyage/UI/AgeActivity.java
@@ -1,9 +1,7 @@
 package org.computermentors.whatsmyage.UI;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -19,19 +17,7 @@ public class AgeActivity extends AppCompatActivity {
         final TextView ageTextView = (TextView) findViewById(R.id.ageTextView);
         final Button backButton = (Button) findViewById(R.id.backButton);
 
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startMainActivity();
-            }
-        });
-
     }
 
-    private void startMainActivity (){
-        Intent intent = new Intent(this,MainActivity.class);
-
-        startActivity(intent);
-    };
 
 }
