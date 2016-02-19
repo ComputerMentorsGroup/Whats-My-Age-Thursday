fd4d968a1f9ceee2a53547b96c8db39dc0ce7872
Casoclash <casoclash@hotmail.com>
2016-02-18 19:31:17 -0500

    changes to date receiving
    this edit allows activity to receive data from Main activity

--- app/src/main/java/org/computermentors/whatsmyage/UI/AgeActivity.java
+++ app/src/main/java/org/computermentors/whatsmyage/UI/AgeActivity.java
@@ -1,7 +1,9 @@
 package org.computermentors.whatsmyage.UI;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,10 +16,27 @@ public class AgeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_age);
 
+        Intent intent = getIntent();
+        int age = intent.getIntExtra("age");
+
         final TextView ageTextView = (TextView) findViewById(R.id.ageTextView);
         final Button backButton = (Button) findViewById(R.id.backButton);
 
+        ageTextView.setText(Integer.toString(int i));
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMainActivity();
+            }
+        });
+
     }
 
-
+    private void startMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 }
+
+
+
