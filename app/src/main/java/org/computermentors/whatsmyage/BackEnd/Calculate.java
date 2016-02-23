7d8d57ae8b40b262e442c852587fce680f92e519
CryoFlar3 <ndukes@computermentors.org>
2016-02-23 17:19:59 -0500

    Beta version
    The app is nearly done. Just need to go through it and make sure there
    are no bugs in the app.

--- app/src/main/java/org/computermentors/whatsmyage/BackEnd/Calculate.java
+++ app/src/main/java/org/computermentors/whatsmyage/BackEnd/Calculate.java
@@ -2,6 +2,7 @@ package org.computermentors.whatsmyage.BackEnd;
 
 import android.widget.DatePicker;
 
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -9,11 +10,25 @@ import java.util.Date;
  */
 public class Calculate {
 
-    Date getDate (DatePicker date){
+    public Date getDate (DatePicker date){
+        Date newDate = new Date(date.getYear() - 1900, date.getMonth(), date.getDayOfMonth());
 
+        return newDate;
     }
 
-    int getAge (Date date){
+    public int getAge (Date date){
 
+        Calendar dob = Calendar.getInstance();
+        dob.setTime(date);
+        Calendar today = Calendar.getInstance();
+        int age = today.get(Calendar.YEAR) - dob.get(Calendar.YEAR);
+        if (today.get(Calendar.MONTH) < dob.get(Calendar.MONTH)) {
+            age--;
+        } else if (today.get(Calendar.MONTH) == dob.get(Calendar.MONTH)
+                && today.get(Calendar.DAY_OF_MONTH) < dob.get(Calendar.DAY_OF_MONTH)) {
+            age--;
+        }
+
+        return age;
     }
 }
--- app/src/main/java/org/computermentors/whatsmyage/UI/MainActivity.java
+++ app/src/main/java/org/computermentors/whatsmyage/UI/MainActivity.java
@@ -1,12 +1,17 @@
 package org.computermentors.whatsmyage.UI;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.Toast;
 
 import org.computermentors.whatsmyage.BackEnd.Calculate;
 import org.computermentors.whatsmyage.R;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +25,24 @@ public class MainActivity extends AppCompatActivity {
         final DatePicker birthDatePicker = (DatePicker) findViewById(R.id.birthDatePicker);
         final Button findAgeButton = (Button) findViewById(R.id.findAgeButton);
 
+        findAgeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Date date = mCalculate.getDate(birthDatePicker);
+
+                int age = mCalculate.getAge(date);
+
+                startAgeActivity(age);
+            }
+        });
+
+    }
+
+    private void startAgeActivity(int age){
+        Intent intent = new Intent(this, AgeActivity.class);
+        intent.putExtra("age", age);
+        startActivity(intent);
+        Toast.makeText(this, "" + age, Toast.LENGTH_LONG).show();
     }
 
 }
